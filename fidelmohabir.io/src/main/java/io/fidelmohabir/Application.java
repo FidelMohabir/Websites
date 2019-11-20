517c5453e080342dbc7184838367349ed555ae0e
FidelMohabir <43044755+FidelMohabir@users.noreply.github.com>
2019-11-20 03:30:28 -0500

    added basic encryption

--- fidelmohabir.io/src/main/java/io/fidelmohabir/Application.java
+++ fidelmohabir.io/src/main/java/io/fidelmohabir/Application.java
@@ -3,8 +3,13 @@ package io.fidelmohabir;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 
 @SpringBootApplication
+@EnableEncryptableProperties
+@PropertySource(name="EncryptedProperties", value = "classpath:application.properties")
 public class Application {
 
 	public static void main(String[] args) {
